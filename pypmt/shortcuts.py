4f8ce6d938fee7ea79c7ca57c83aed18b38a0860
Mustafa Faisal <mustafafaisal@Mustafas-MacBook-Pro.local>
2024-05-20 22:02:16 +0100

    removing the up into a separate repo.

--- pypmt/shortcuts.py
+++ pypmt/shortcuts.py
@@ -13,8 +13,3 @@ from pypmt.planner.QFUF import QFUFSearch
 from pypmt.config import config
 
 from pypmt.shared.valid_configs import valid_configs
-
-# Register planner to UP framework
-from .up.SMTPlanner import SMTPlanner
-env = up.environment.get_environment()
-env.factory.add_engine('SMTPlanner', __name__, 'SMTPlanner')
