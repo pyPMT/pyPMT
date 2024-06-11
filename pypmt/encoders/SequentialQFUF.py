ba91823f23d95960a7518e3eec8be41ecd57595b
MFaisalZaki <mustafa.faisal.eng@gmail.com>
2024-06-11 15:07:37 +0100

    fixing the stuipd context mismatch for QFUF.

--- pypmt/encoders/SequentialQFUF.py
+++ pypmt/encoders/SequentialQFUF.py
@@ -326,7 +326,7 @@ class EncoderSequentialQFUF(Encoder):
 
             # for an action to be executable, it needs to be selected
             # and the types need to be correct
-            if all(len(action.children()) == 0 for action in [action_typing, action_pre, action_eff]):
+            if any(len(action.children()) == 0 for action in [action_typing, action_pre, action_eff]):
                 action = z3.Implies(action_matches, z3.BoolVal(True, ctx=self.ctx))
             else:
                 action = z3.Implies(z3.And([action_matches]), 
